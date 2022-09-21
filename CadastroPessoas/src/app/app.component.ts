bab3590a0019c91eb4a6d3ce02e6ae6feca57c72
pedropalombo <pedro.palombo@hotmail.com>
2022-09-21 17:44:25 -0300

    Extra CSS / Basic TS & Structure

--- CadastroPessoas/src/app/app.component.ts
+++ CadastroPessoas/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pessoa } from './pessoa';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CadastroPessoas';
+
+  public pessoaNome : String = "ulala";
+  public pessoaLista : Array<Pessoa> = [];
+
+  ngOnInit(): void {
+    this.pessoaLista = [
+      {
+        id: 11111,
+        nome: 'ullll',
+        email: 'ula@lol.com',
+        dataNasc: undefined,
+        fotoURL: undefined,
+      },
+      {
+        id: 3578,
+        nome: 'pax',
+        email: 'saas@jooj.com',
+        dataNasc: undefined,
+        fotoURL: undefined,
+      },
+      {
+        id: 616,
+        nome: 'magnesio',
+        email: 'fermentacao@quimica.com',
+        dataNasc: undefined,
+        fotoURL: undefined,
+      },
+      {
+        id: 777,
+        nome: 'deusvult',
+        email: 'inri@bless.com',
+        dataNasc: undefined,
+        fotoURL: undefined,
+      }
+    ]
+  }
 }
