11eb6b71bf054e88ffd724bc2bf5031721d9f512
pedropalombo <pedro.palombo@hotmail.com>
2022-09-21 16:39:46 -0300

    Main Page Basic Structure

--- CadastroPessoas/src/app/app.module.ts
+++ CadastroPessoas/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ListarPessoasComponent } from './listar-pessoas/listar-pessoas.component';
+import { CadastrarPessoasComponent } from './cadastrar-pessoas/cadastrar-pessoas.component';
+import { MostrarPerfilComponent } from './mostrar-perfil/mostrar-perfil.component';
+import { EditarPessoaComponent } from './editar-pessoa/editar-pessoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListarPessoasComponent,
+    CadastrarPessoasComponent,
+    MostrarPerfilComponent,
+    EditarPessoaComponent
   ],
   imports: [
     BrowserModule,
---
+++ CadastroPessoas/src/app/cadastrar-pessoas/cadastrar-pessoas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar-pessoas',
+  templateUrl: './cadastrar-pessoas.component.html',
+  styleUrls: ['./cadastrar-pessoas.component.scss']
+})
+export class CadastrarPessoasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
